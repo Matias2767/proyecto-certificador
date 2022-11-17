05a400248403e5d1c152665c6ca7d3552666be4b
jirolp <jirolpdanielo@gmail.com>
2022-11-16 19:26:02 -0500

    registro

--- controllers/crud.js
+++ controllers/crud.js
@@ -1,17 +1,28 @@
 const conexion = require('../database/db');
-exports.addalumnos = (req,res)=>{
-    const cod = req.body.cod;
-    const ape = req.body.ape;
+exports.addusuario = (req,res)=>{
     const nom = req.body.nom;
-    const dir = req.body.dir;
-    const telef = req.body.telef;
-    const cod_carrera = req.body.cod_carrera;
-    conexion.query('insert into alumnos set ?',{IdAutor:cod,
-    Apellidos:ape,Nombres:nom,Direccion:dir,Telefono:telef,IdCarrera:cod_carrera},(error)=>{
+    const ape = req.body.ape;
+    let car = req.body.car;
+    const user = req.body.user;
+    const pass = req.body.pass;
+    /*const id_car = String(conexion.query('select id_cargo from cargo where cargo.nombre = ?', {nombre:car},(error)=>{
         if(error){
             console.log(error);
         }else{
-            res.redirect('/mr_alumnos');
+            res.redirect('/registro');
+        }
+    }));*/ 
+    if(car == "Vendedor"){
+        car = "001";
+    }else{
+        car = "002";
+    }
+    conexion.query('insert into usuarios set ?',{nombre:nom,
+    apellido:ape,id_cargo:car,username:user,password:pass},(error)=>{
+        if(error){
+            console.log(error);
+        }else{
+            res.redirect('/registro');
         }
     });
 }
--- router.js
+++ router.js
@@ -26,9 +26,9 @@ router.get('/mr_alumnos',(req,res)=>{
     });
 });
 
-//ruta para nuevos alumnos
-router.get('/mr_alumnosNuevo',(req,res)=>{
-    res.render('mr_alumnosNuevo');
+//ruta para nuevos usuarios
+router.get('/registro',(req,res)=>{
+    res.render('registro');
 });
 
 //ruta para editar alumnos
@@ -55,9 +55,9 @@ router.get('/mr_alumnosdelete/:id',(req,res)=>{
     });
 });
 
-//ruta para nuevos empleados
+//ruta para nuevos usuarios
 const crud = require('./controllers/crud');
-router.post('/addalumnos',crud.addalumnos);
+router.post('/addusuario',crud.addusuario);
 //ruta para editar empleados
 router.post('/updatealumnos',crud.updatealumnos);
 
